cf32cf5a434226a58d61db17fa514449c4110ec7
Akash7210 <akashpanchal2203@gmail.com>
2024-04-03 09:52:28 +0530

    Added bcrypt method to password and jwt token.

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,7 +8,8 @@ const UserSchema = new Schema({
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique  :true
         
     },
     password:{
@@ -22,4 +23,5 @@ const UserSchema = new Schema({
 
  
 });
-module.exports = mongoose.model('user',UserSchema);
+ const User = mongoose.model('user',UserSchema);
+ module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,37 +1,69 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-// Create a user using POST "/api/auth", doesn't require authentication
-router.post('/', [
-  body('name', 'Enter a valid name').isLength({ min: 3 }),
-  body('email', "Enter a valid Email").isEmail(),
-  body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+const JWT_SECRET ='Akashisagood@boy';
 
-  try {
-    const user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    res.json(user);
-  } catch (error) {
-    if (error.code === 11000) {
-      // Duplicate key error
-      return res.status(400).json({ error: 'Email already exists' });
+
+// Create a user using POST "/api/auth", doesn't require authentication . No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must have a minimum of 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //If there are error , return Bad request and the errors.
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    console.error(error);
-    res.status(500).json({ error: 'Server error' });
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "Sorry a user with this email exixts" });
+    }
+
+    try {
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password,salt);
+      
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user:{
+          id: user.id
+         
+        }
+      }
+
+      const authtoken = jwt.sign(data,JWT_SECRET);
+    
+
+      //res.json(user);
+      res.json({authtoken})
+
+    } catch (error) {
+      if (error.code === 11000) {
+        // Duplicate key error
+        return res.status(400).json({ error: "Email already exists" });
+      }
+      console.error(error);
+      res.status(500).json({ error: "Server error" });
+    }
   }
-});
+);
 
 module.exports = router;
-
-module.exports = router
-
